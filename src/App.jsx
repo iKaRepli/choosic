c00b4a725365baaaa63bd5a4ca76205ae07e0760
ikarepli <chaqui@live.com.mx>
2024-03-08 02:12:37 -0700

    Agregamos Tailwind y pagina principal

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './index.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import imagen from './1.jpeg'
 
+export default function App() {
   return (
-    <>
+    <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
+      <div className="shrink-0">
+        <img className="h-12 w-12" src={imagen} alt="hola"/>
+      </div>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <div className="text-xl font-medium text-black">hola</div>
+        <p className="text-slate-500">asdasd</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
-export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { Link } from 'react-router-dom';
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+
+<div className="my-container h-screen flex items-center justify-center bg-slate-900">
+      <div className="text-center">
+        <h1 className="text-3xl font-bold mb-4">Bienvenido a Choosic</h1>
+        <p className="text-gray-600 mb-8">Encuentra salas y chatea en tiempo real</p>
+        <a to="/ingresar" className="bg-blue-500 text-white px-4 py-2 rounded-md mr-4">Ingresar a Sala</a>
+        <a to="/crear-sala" className="bg-green-500 text-white px-4 py-2 rounded-md mr-4">Crear Sala</a>
+        <a to="/login" className="bg-gray-500 text-white px-4 py-2 rounded-md">Iniciar Sesión</a>
+      </div>
+    </div>
+   
   </React.StrictMode>,
 )
